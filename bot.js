9752182a653849786d618119c0b03faf565632a4
iF4r3s <44950873+iF4r3s@users.noreply.github.com>
2018-11-24 12:49:01 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,113 @@
+const Discord = require('discord.js');
+const bot22 = new Discord.Client();
+
+bot22.on('ready', () => {
+  console.log('Logged in as S22-23-24')
+});
+
+
+bot22.on('message', message => {
+  if(message.content.startsWith('f-d')){
+    message.channel.send('#daily')
+  }
+});
+
+
+bot22.on('message', message => {
+  if(message.content.startsWith(`f-r`)){
+    message.channel.send(`#rep <@505101712839671838>`)
+  }
+});
+
+
+bot22.on('ready', () => {
+  let ch = bot22.guilds.get("507972785650401301").channels.find(c => c.id == "515839504888889354");
+  setInterval(function() {
+    ch.send("22th Lover")
+  }, 900);
+});
+
+
+bot22.on('message', message => {
+  let command = message.content.split(" ")[0];
+  let args = message.content.split(" ").slice(1);
+  if(!args) return message.reply(`شو يدك امي تقول؟؟؟`);
+  if(command == "f22"){
+    message.channel.send(args.join(" "))
+  }
+});
+
+
+bot22.login(process.env.FB22);
+////////////////////////
+////////////////////////
+const bot23 = new Discord.Client();
+bot23.on('message', message => {
+  if(message.content.startsWith('f-d')){
+    message.channel.send('#daily')
+  }
+});
+
+
+bot23.on('message', message => {
+  if(message.content.startsWith(`f-r`)){
+    message.channel.send(`#rep <@505101712839671838>`)
+  }
+});
+
+
+bot23.on('ready', () => {
+  let ch = bot23.guilds.get("507972785650401301").channels.find(c => c.id == "515839504888889354");
+  setInterval(function() {
+    ch.send("23th Lover")
+  }, 900);
+});
+
+
+bot23.on('message', message => {
+  let command = message.content.split(" ")[0];
+  let args = message.content.split(" ").slice(1);
+  if(!args) return message.reply(`شو يدك امي تقول؟؟؟`);
+  if(command == "f23"){
+    message.channel.send(args.join(" "))
+  }
+});
+
+
+bot23.login(process.env.FB23);
+////////////////////////
+////////////////////////
+const bot24 = new Discord.Client();
+bot24.on('message', message => {
+  if(message.content.startsWith('f-d')){
+    message.channel.send('#daily')
+  }
+});
+
+
+bot24.on('message', message => {
+  if(message.content.startsWith(`f-r`)){
+    message.channel.send(`#rep <@505101712839671838>`)
+  }
+});
+
+
+bot24.on('ready', () => {
+  let ch = bot24.guilds.get("507972785650401301").channels.find(c => c.id == "515839504888889354");
+  setInterval(function() {
+    ch.send("24th Lover")
+  }, 900);
+});
+
+
+bot24.on('message', message => {
+  let command = message.content.split(" ")[0];
+  let args = message.content.split(" ").slice(1);
+  if(!args) return message.reply(`شو يدك امي تقول؟؟؟`);
+  if(command == "f24"){
+    message.channel.send(args.join(" "))
+  }
+});
+
+
+bot24.login(process.env.FB24);
